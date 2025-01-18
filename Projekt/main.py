e0c29078057814b348e797e846277e3468999a83
PawelHermansdorfer <pawelhermansdorfer@gmail.com>
2025-01-19 00:55:51 +0100

    Update

--- Projekt/main.py
+++ Projekt/main.py
@@ -1,4 +1,3 @@
-# TODO(Pawel Hermansdorfer): HUge fitnes boost for pasing pipe
 # TODO(Pawel Hermansdorfer): Fitness decrease when hit ground
 # TODO(Pawel Hermansdorfer): punish going over the top of the screen
 # https://www.pygame.org/wiki/MatplotlibPygame
@@ -18,24 +17,54 @@ from OpenGL.GLU import *
 import imgui
 from imgui.integrations.pygame import PygameRenderer
 
+GAME_STARTED = False
+
 ########################################
 # Setup params
-HIDDEN_NEURON_COUNT = 5
+all_possible_inputs = [
+        'None',
+
+        'Bird\'s position on Y axis',
+        'Bird\'s position on X axis',
+        'Bird\'s velocity on Y axis',
+
+        'Next pipe\'s position on X axis',
+        'Next gaps\'s position on Y axis',
+
+        'Bird\'s distance to next pipe',
+        'Difference between bird and next gap on Y axis',
+]
+selected_inputs = [['', 0] for _ in all_possible_inputs]
+selected_inputs[0] = [all_possible_inputs[1], 1]
+selected_inputs[1] = [all_possible_inputs[4], 4]
+
+nn_inputs_order = []
+
+input_count = 4
+hidden_neuron_count = 5
+
 normalize_inputs = False
 
-punish_for_hitting_ground = False
-
-ELITISM = True
 mutation_propability = 0.5
 mutation_magnitude = 0.5
-
 crossover_propability = 0.5
+
+elitism = True
+punish_hitting_ground = False
+punish_flying_above_screen = False
 
 # Settings
 game_speed_factor = 1
+draw_hitboxes = False
+hitboxes_width = 2
+display_fps = False
 max_fps = 30
 max_fps_options = [30, 60, 120, 144, 164]
 max_fps_option_idx = 0
+
+# UI error messages 
+failed_no_inputs = False
+failed_zero_probabilities = False
 
 ######################################## Load data
 window = pygame.display.set_mode((0,0), pygame.DOUBLEBUF | pygame.OPENGL)
@@ -151,17 +180,12 @@ current_generation_idx = 0
 best_fitness_this_generation = 0
 best_fitness_overall = 0
 
-input_count = 4
-hidden_neuron_count = HIDDEN_NEURON_COUNT
 input_values  = [np.array([0, 0, 0, 0]) for _ in range(BIRD_COUNT)]
 hidden_weights = [np.random.normal(size=(input_count, hidden_neuron_count)) for _ in range(BIRD_COUNT)]
 hidden_biases  = [np.random.normal(size=hidden_neuron_count) for _ in range(BIRD_COUNT)]
 hidden_values = [np.array([0 for _ in range(hidden_neuron_count)]) for __ in range(BIRD_COUNT)]
 output_weights = [np.random.normal(size=hidden_neuron_count) for _ in range(BIRD_COUNT)]
 output_value = [0 for _ in range(BIRD_COUNT)]
-
-# Genetic algorithm params
-elitism = ELITISM
 
 # Pipes
 pipe_width  = pipe_texture.get_size()[0] * texture_to_world_scale
@@ -179,6 +203,30 @@ pipes_dx = background_half_width + pipe_half_width
 pipes_spawn_x = right_side_of_background + pipe_half_width
 first_pipe_x = pipes_spawn_x
 pipes_y = [get_new_pipe_y(), get_new_pipe_y()]
+
+def blit(texture, pos, angle=0):
+    texture_dim = texture.get_size()
+    texture = pygame.transform.scale(texture, (texture_dim[0]*game_scale[0],
+                                               texture_dim[1]*game_scale[1]))
+    if angle != 0:
+        texture = pygame.transform.rotate(texture, angle)
+    rect = texture.get_rect(center=world_to_screen(pos))
+    game_surface.blit(texture, rect)
+
+def draw_rect_hitbox(center, w, h):
+    w = w * (1/texture_to_world_scale * game_scale[0])
+    h = h * (1/texture_to_world_scale * game_scale[0])
+    center = world_to_screen(center)
+    rect = pygame.Rect(center[0] - w//2, center[1]-h//2, w, h)
+    pygame.draw.rect(game_surface, (255, 0, 0), rect, hitboxes_width)
+
+def get_node_or_weight_color(v):
+    r = min(max(v, -1), 0) * -255
+    g = min(max(v, 0), 1) * 255
+    return (r, g, 0)
+
+def generate_first_population():
+    pass
 
 ######################################## Game loop
 stoped = False
@@ -203,13 +251,20 @@ while not stoped:
         impl.process_event(event)
 
     ######################################## Update
-    if frame_time != 0:
+    if frame_time != 0 and GAME_STARTED:
         dt = frame_time
 
         for _ in range(game_speed_factor):
             for bird_idx in range(BIRD_COUNT):
                 if alive[bird_idx]:
-                    fitness[bird_idx] += 1
+
+                    if punish_flying_above_screen:
+                        if birds_y[bird_idx] <= background_half_height:
+                            fitness[bird_idx] += 1
+                    else:
+                        fitness[bird_idx] += 1
+
+
                     if best_fitness_this_generation < fitness[bird_idx]:
                         best_fitness_this_generation = fitness[bird_idx]
 
@@ -270,7 +325,7 @@ while not stoped:
                     # Handle bird-ground collision
                     if birds_y[bird_idx] - bird_hitbox_r <= ground_y + ground_half_height:
                         die(bird_idx) # make death
-                        if punish_for_hitting_ground:
+                        if punish_hitting_ground:
                             fitness[bird_idx] = 0
 
                     # Bird
@@ -361,107 +416,111 @@ while not stoped:
 
     ######################################## Draw
     window_surface.fill((33,33,33))
-    game_surface.fill((33,33,33))
 
-    def blit(texture, pos, angle=0):
-        texture_dim = texture.get_size()
-        texture = pygame.transform.scale(texture, (texture_dim[0]*game_scale[0],
-                                                   texture_dim[1]*game_scale[1]))
-        texture = pygame.transform.rotate(texture, angle)
-        rect = texture.get_rect(center=world_to_screen(pos))
-        game_surface.blit(texture, rect)
+    if GAME_STARTED:
+        game_surface.fill((33,33,33))
 
-    # background
-    blit(bg_texture, (0, 0), 0)
+        # background
+        blit(bg_texture, (0, 0), 0)
 
-    # Pipes
-    blit(pipe_texture, (first_pipe_x, pipes_y[0]), 180)
-    blit(pipe_texture, (first_pipe_x, pipes_y[0] - gap_between_pipes), 0)
+        # Pipes
+        blit(pipe_texture, (first_pipe_x, pipes_y[0]), 180)
+        blit(pipe_texture, (first_pipe_x, pipes_y[0] - gap_between_pipes), 0)
+        if draw_hitboxes:
+            draw_rect_hitbox((first_pipe_x, pipes_y[0]), pipe_width, pipe_height)
+            draw_rect_hitbox((first_pipe_x, pipes_y[0] - gap_between_pipes), pipe_width, pipe_height)
 
-    blit(pipe_texture, (first_pipe_x + pipes_dx, pipes_y[1]), 180)
-    blit(pipe_texture, (first_pipe_x + pipes_dx, pipes_y[1] - gap_between_pipes), 0)
+        blit(pipe_texture, (first_pipe_x + pipes_dx, pipes_y[1]), 180)
+        blit(pipe_texture, (first_pipe_x + pipes_dx, pipes_y[1] - gap_between_pipes), 0)
 
-    # Ground
-    blit(ground_texture, (ground_x, ground_y))
-    blit(ground_texture, (ground_x + 2*ground_half_width - 0.005, ground_y)) # cheesy -0.005 to be sure there's no pixel gap between ground tiles
+        if draw_hitboxes:
+            draw_rect_hitbox((first_pipe_x + pipes_dx, pipes_y[1]), pipe_width, pipe_height)
+            draw_rect_hitbox((first_pipe_x + pipes_dx, pipes_y[1] - gap_between_pipes), pipe_width, pipe_height)
 
-    # Bird
-    # NOTE(Pawel Hermansdorfer): Draw reversed so that idx 0 - best from prev gen with elitism (red bird) is drawn at the top
-    for bird_idx in range(BIRD_COUNT-1, 0-1, -1):
-        if alive[bird_idx]:
-            bird_color = birds_colors[bird_idx]
-            bird_texture_idx = int(frame_start*7 % len(bird_textures))
-            bird_texture = bird_textures[bird_color][bird_texture_idx]
-            bird_angle = max(min(bird_vel[bird_idx]*3.4 + 8, 4), -4) * 8
-            blit(bird_texture, [birds_x, birds_y[bird_idx]], bird_angle)
+        # Ground
+        blit(ground_texture, (ground_x, ground_y))
+        blit(ground_texture, (ground_x + 2*ground_half_width - 0.005, ground_y)) # cheesy -0.005 to be sure there's no pixel gap between ground tiles
 
-    # Game surface
-    window_surface.blit(game_surface, (0, 0))
+        if draw_hitboxes:
+            draw_rect_hitbox((ground_x, ground_y), ground_width, ground_height)
+            draw_rect_hitbox((ground_x + 2*ground_half_width - 0.005, ground_y), ground_width, ground_height)
 
-    # Neural network
-    input_nodes_positions  = []
-    hidden_nodes_positions = []
-    output_nodes_positions = []
+        # Bird
+        # NOTE(Pawel Hermansdorfer): Draw reversed so that idx 0 - best from prev gen with elitism (red bird) is drawn at the top
+        for bird_idx in range(BIRD_COUNT-1, 0-1, -1):
+            if alive[bird_idx]:
+                bird_color = birds_colors[bird_idx]
+                bird_texture_idx = int(frame_start*7 % len(bird_textures))
+                bird_texture = bird_textures[bird_color][bird_texture_idx]
+                bird_angle = max(min(bird_vel[bird_idx]*3.4 + 8, 4), -4) * 8
+                blit(bird_texture, [birds_x, birds_y[bird_idx]], bird_angle)
 
-    best_bird_idx = np.argmax(fitness) # if couple has the same, best one with elitism is best from prev gen pog!
-    input_node_count = input_values[best_bird_idx].shape[0]
-    input_nodes_x = ((window_dim[0] - game_dim[0]) / 4) + game_dim[0]
-    input_node_r = 20
-    space_between_input_nodes = 60
-    y = game_dim[1]/2 - (input_node_count - 1)*(2*input_node_r + space_between_input_nodes)*0.5
-    for i in range(input_node_count):
-        input_nodes_positions.append((input_nodes_x, y))
-        y += 2*input_node_r + space_between_input_nodes
+                if draw_hitboxes:
+                    pygame.draw.circle(game_surface, (255, 0, 0), world_to_screen([birds_x, birds_y[bird_idx]]),
+                                       bird_half_width * (1/texture_to_world_scale * game_scale[0]),
+                                       hitboxes_width)
 
-    hidden_node_x = ((window_dim[0] - game_dim[0]) / 2) + game_dim[0]
-    space_between_hidden_nodes = 60
-    hidden_node_r = 20
-    y = game_dim[1]/2 - (hidden_neuron_count - 1)*(2*hidden_node_r + space_between_hidden_nodes)*0.5
-    for i in range(hidden_neuron_count):
-        hidden_nodes_positions.append((hidden_node_x, y))
-        y += 2*hidden_node_r + space_between_hidden_nodes
+        # Game surface
+        window_surface.blit(game_surface, (0, 0))
 
-    output_x = (3*(window_dim[0] - game_dim[0])/4) + game_dim[0]
-    output_y = game_dim[1]/2
-    output_node_pos = (output_x, output_y)
-    output_r = 20
+        # Neural network
+        input_nodes_positions  = []
+        hidden_nodes_positions = []
+        output_nodes_positions = []
 
-    def get_node_or_weight_color(v):
-        r = min(max(v, -1), 0) * -255
-        g = min(max(v, 0), 1) * 255
-        return (r, g, 0)
+        best_bird_idx = np.argmax(fitness) # if couple has the same, best one with elitism is best from prev gen pog!
+        input_node_count = input_values[best_bird_idx].shape[0]
+        input_nodes_x = ((window_dim[0] - game_dim[0]) / 4) + game_dim[0]
+        input_node_r = 20
+        space_between_input_nodes = 60
+        y = game_dim[1]/2 - (input_node_count - 1)*(2*input_node_r + space_between_input_nodes)*0.5
+        for i in range(input_node_count):
+            input_nodes_positions.append((input_nodes_x, y))
+            y += 2*input_node_r + space_between_input_nodes
 
-    for i, input_node_pos in enumerate(input_nodes_positions):
-        for j, hidden_node_pos in enumerate(hidden_nodes_positions):
-            weight_color = get_node_or_weight_color(hidden_weights[best_bird_idx][i, j])
-            weight_width = int(np.ceil(np.abs(hidden_weights[best_bird_idx][i, j]) * 1.5 + 1))
-            pygame.draw.line(window_surface, weight_color, input_node_pos, hidden_node_pos, width=weight_width)
-        input_color = get_node_or_weight_color(input_values[best_bird_idx][i])
-        pygame.draw.circle(window_surface, input_color, input_node_pos, radius=hidden_node_r)
+        hidden_node_x = ((window_dim[0] - game_dim[0]) / 2) + game_dim[0]
+        space_between_hidden_nodes = 60
+        hidden_node_r = 20
+        y = game_dim[1]/2 - (hidden_neuron_count - 1)*(2*hidden_node_r + space_between_hidden_nodes)*0.5
+        for i in range(hidden_neuron_count):
+            hidden_nodes_positions.append((hidden_node_x, y))
+            y += 2*hidden_node_r + space_between_hidden_nodes
 
-    for i, hidden_node_pos in enumerate(hidden_nodes_positions):
-        weight_color = get_node_or_weight_color(output_weights[best_bird_idx][i])
-        weight_width = int(np.ceil(np.abs(output_weights[best_bird_idx][i]) * 1.5 + 1))
-        pygame.draw.line(window_surface, weight_color, hidden_node_pos, output_node_pos, width=weight_width)
-        hidden_color = get_node_or_weight_color(hidden_values[best_bird_idx][i])
-        pygame.draw.circle(window_surface, hidden_color, hidden_node_pos, radius=hidden_node_r)
+        output_x = (3*(window_dim[0] - game_dim[0])/4) + game_dim[0]
+        output_y = game_dim[1]/2
+        output_node_pos = (output_x, output_y)
+        output_r = 20
 
-    output_color = get_node_or_weight_color(output_value[best_bird_idx])
-    pygame.draw.circle(window_surface, output_color, output_node_pos, radius=hidden_node_r)
+        for i, input_node_pos in enumerate(input_nodes_positions):
+            for j, hidden_node_pos in enumerate(hidden_nodes_positions):
+                weight_color = get_node_or_weight_color(hidden_weights[best_bird_idx][i, j])
+                weight_width = int(np.ceil(np.abs(hidden_weights[best_bird_idx][i, j]) * 1.5 + 1))
+                pygame.draw.line(window_surface, weight_color, input_node_pos, hidden_node_pos, width=weight_width)
+            input_color = get_node_or_weight_color(input_values[best_bird_idx][i])
+            pygame.draw.circle(window_surface, input_color, input_node_pos, radius=hidden_node_r)
 
-    # Telemetry 
-    telemetry = [
-            f'Frame time:  {frame_time*1000:.1f}ms',
-            f'FPS: {(1/frame_time) if frame_time != 0 else 0:.0f}',
-    ]
-    y = 0
-    for line in telemetry:
-        window_surface.blit(debug_font.render(line, True, (255,255,255)), (0, y))
-        y += 30
+        for i, hidden_node_pos in enumerate(hidden_nodes_positions):
+            weight_color = get_node_or_weight_color(output_weights[best_bird_idx][i])
+            weight_width = int(np.ceil(np.abs(output_weights[best_bird_idx][i]) * 1.5 + 1))
+            pygame.draw.line(window_surface, weight_color, hidden_node_pos, output_node_pos, width=weight_width)
+            hidden_color = get_node_or_weight_color(hidden_values[best_bird_idx][i])
+            pygame.draw.circle(window_surface, hidden_color, hidden_node_pos, radius=hidden_node_r)
 
+        output_color = get_node_or_weight_color(output_value[best_bird_idx])
+        pygame.draw.circle(window_surface, output_color, output_node_pos, radius=hidden_node_r)
+
+        # Telemetry 
+        if display_fps:
+            telemetry = [
+                    f'Frame time:  {frame_time*1000:.1f}ms',
+                    f'FPS: {(1/frame_time) if frame_time != 0 else 0:.0f}',
+            ]
+            y = 0
+            for line in telemetry:
+                window_surface.blit(debug_font.render(line, True, (255,255,255)), (0, y))
+                y += 30
 
     # Draw to window
-    # window.blit(window_surface, (0, 0)) # Withouth opengl backend
     texture_data = pygame.image.tostring(window_surface, "RGBA", 1)
     width, height = window_surface.get_width(), window_surface.get_height()
     
@@ -492,28 +551,102 @@ while not stoped:
 
     # UI
     imgui.new_frame()
+    if GAME_STARTED:
+        imgui.begin("Settings & Stats")
 
-    imgui.begin("Settings & Stats")
+        imgui.text(f'Generation: {current_generation_idx}')
+        imgui.text(f'Birds alive: {alive_count}')
+        imgui.text(f'Best fitness this generation: {best_fitness_this_generation}')
+        imgui.text(f'Best fitness overall: {best_fitness_overall}')
 
-    imgui.text(f'Generation: {current_generation_idx}')
-    imgui.text(f'Birds alive: {alive_count}')
-    imgui.text(f'Best fitness this generation: {best_fitness_this_generation}')
-    imgui.text(f'Best fitness overall: {best_fitness_overall}')
+        imgui.spacing()
 
-    imgui.separator()
+        if imgui.button('Skip generation'):
+            for bird_idx in range(BIRD_COUNT):
+                die(bird_idx)
 
-    _, game_speed_factor = imgui.slider_int("Game speed", game_speed_factor, 0, 15)
-    max_fps_change, max_fps_option_idx = imgui.combo("Max FPS", max_fps_option_idx, ['30', '60', '120', '144', '165'])
-    if max_fps_change:
-        max_fps = max_fps_options[max_fps_option_idx]
+        imgui.separator()
 
-    if imgui.button('Skip generation'):
-        for bird_idx in range(BIRD_COUNT):
-            die(bird_idx)
+        _, game_speed_factor = imgui.slider_int("Game speed", game_speed_factor, 0, 20)
 
-    if imgui.button('QUIT'):
-        stoped = True
-    imgui.end()
+        imgui.spacing()
+
+        _, display_fps = imgui.checkbox('Show FPS', display_fps)
+        max_fps_change, max_fps_option_idx = imgui.combo("Max FPS", max_fps_option_idx, ['30', '60', '120', '144', '165'])
+        if max_fps_change:
+            max_fps = max_fps_options[max_fps_option_idx]
+
+        imgui.spacing()
+
+        _, draw_hitboxes = imgui.checkbox('Show Hitboxes', draw_hitboxes)
+        _, hitboxes_width = imgui.slider_int("Hitboxes width", hitboxes_width, 0, 5)
+
+        imgui.spacing()
+
+        if imgui.button('QUIT'):
+            stoped = True
+        imgui.end()
+    else:
+        imgui.begin("Setup")
+
+
+        possible_inputs = all_possible_inputs.copy()
+        for i in range(len(all_possible_inputs) - 1):
+            _, selected_input_idx = imgui.combo(f"Select input {i+1}", selected_inputs[i][1], possible_inputs)
+            if selected_input_idx == 0:
+                for j in range(i, len(selected_inputs)):
+                    selected_inputs[j] = ['', 0]
+                break
+            else:
+                selected_inputs[i][0] = possible_inputs[selected_input_idx]
+                selected_inputs[i][1] = selected_input_idx
+                possible_inputs.remove(selected_inputs[i][0])
+
+        imgui.spacing()
+
+        _, normalize_inputs = imgui.checkbox('Normalize inputs', normalize_inputs)
+
+        imgui.spacing()
+
+        _, hidden_neuron_count = imgui.input_int('Hidden layer size', hidden_neuron_count)
+
+        imgui.separator()
+
+        _, BIRD_COUNT = imgui.input_int('Population size', BIRD_COUNT)
+
+        _, elitism = imgui.checkbox('Elitism', elitism)
+
+        _, mutation_propability = imgui.slider_float("Mutation propablility", mutation_propability, 0, 1)
+        _, mutation_magnitude = imgui.slider_float("Mutation magnitude", mutation_magnitude, 0, 2)
+        _, crossover_propability = imgui.slider_float("Crossover propablility", crossover_propability, 0, 1)
+
+        _, punish_hitting_ground = imgui.checkbox('Punish for hittin ground', punish_hitting_ground)
+        _, punish_flying_above_screen = imgui.checkbox('Punish for flying above screen', punish_flying_above_screen)
+
+        if imgui.button('START SIMULATION'):
+            failed = False
+            if input_count == 0:
+                failed = True
+                failed_no_inputs = True
+            if mutation_propability == 0 and crossover_propability == 0:
+                failed = True
+                failed_zero_probabilities = True
+
+            if not failed:
+                GAME_STARTED = True
+                for selected_input in selected_inputs:
+                    if selected_input[1] == 0: break
+                    else: nn_inputs_order.append(selected_input[0])
+                generate_first_population()
+
+        imgui.indent()
+        if failed_no_inputs:
+            imgui.text('You must select at least one input')
+
+        if failed_zero_probabilities:
+            imgui.text('At least one of probabilities must be non-zero')
+
+        imgui.end()
 
     imgui.render()
     impl.render(imgui.get_draw_data())
